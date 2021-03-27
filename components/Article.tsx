1e3cd02f4aeafeb208db4363411767f4e73c3958
birros <birros@protonmail.com>
2021-03-27 17:25:22 +0100

    feat: use more dynamic imports

--- components/Article.tsx
+++ components/Article.tsx
@@ -5,24 +5,31 @@ import { Heading, Box } from '@chakra-ui/react'
 import ChakraUIRenderer from '../lib/ChakraUIRenderer'
 import { ApiProps } from '../lib/api'
 import Infos from './Infos'
+import useKatex from '../lib/useKatex'
+import useGraphViz from '../lib/useGraphViz'
 
 export type ArticleProps = Omit<Omit<ApiProps, 'summary'>, 'slug'>
 
-const Article: React.FC<ArticleProps> = ({ title, content, author, date }) => (
-  <Box as="article">
-    <Box as="header">
-      <Heading as="h1">{title}</Heading>
-      <Infos date={date} author={author} mt="2" />
+const Article: React.FC<ArticleProps> = ({ title, content, author, date }) => {
+  useKatex()
+  useGraphViz()
+
+  return (
+    <Box as="article">
+      <Box as="header">
+        <Heading as="h1">{title}</Heading>
+        <Infos date={date} author={author} mt="2" />
+      </Box>
+      <Box as="div" mt={3}>
+        <ReactMarkdown
+          renderers={ChakraUIRenderer()}
+          plugins={[gfm]}
+          children={content}
+          escapeHtml={false}
+        />
+      </Box>
     </Box>
-    <Box as="div" mt={3}>
-      <ReactMarkdown
-        renderers={ChakraUIRenderer()}
-        plugins={[gfm]}
-        children={content}
-        escapeHtml={false}
-      />
-    </Box>
-  </Box>
-)
+  )
+}
 
 export default Article
