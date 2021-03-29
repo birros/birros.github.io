6d3ab067a005897a459f8894b13914656e964c97
birros <birros@protonmail.com>
2021-03-29 12:31:33 +0200

    feat(_app): add seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,39 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/Layout'
 import Head from 'next/head'
 import theme from '../lib/theme'
-import { useSetupVH } from '../lib/hooks'
+import { useSetupVH, useTranslate } from '../lib/hooks'
 import isMobileOrTablet from '../lib/isMobileOrTablet'
 import setupScrollIntoViewPolyfill from '../lib/setupScrollIntoViewPolyfill'
+import { NextSeo } from 'next-seo'
 
 setupScrollIntoViewPolyfill()
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   useSetupVH(!isMobileOrTablet())
+  const _ = useTranslate()
 
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
       </Head>
+      <NextSeo
+        openGraph={{
+          url: _('opengraph.url'),
+          title: _('default.title'),
+          site_name: _('default.title'),
+          type: 'website',
+          description: _('opengraph.description'),
+          images: [
+            {
+              url: 'https://julienmuret.fr/assets/home/screenshot.png',
+              width: 2880,
+              height: 1648,
+              alt: _('opengraph.imageAlt'),
+            },
+          ],
+        }}
+      />
       <ChakraProvider theme={theme}>
         <Layout>
           <Component {...pageProps} />
