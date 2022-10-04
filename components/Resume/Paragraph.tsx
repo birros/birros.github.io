c0820aa335f8bf9472537e84696292be829c1949
birros <birros@protonmail.com>
2022-10-04 15:49:26 +0200

    feat(resume): set text-align to left

--- components/Resume/Paragraph.tsx
+++ components/Resume/Paragraph.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 
 const Paragraph = styled.p`
   font-size: 1.2rem;
-  text-align: justify;
+  text-align: left;
   word-spacing: 0px;
   line-height: 1.6rem;
 `
